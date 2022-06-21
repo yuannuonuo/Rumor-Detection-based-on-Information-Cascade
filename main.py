608600a6fd2618a87e6322e831c0179855769461
Acstream <yuangyue@qq.com>
2022-06-21 11:04:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,56 @@
 
 import sys
 import DBCN
+import BiLSTM
 import DBCN_BiLSTM
 import tensorflow as tf
 import DataPreprocessor1
+import DataPreprocessor11
+import DataPreprocessor2
+import DataPreprocessor3
+import DataPreprocessor4
+import DataPreprocessor41
+import DataPreprocessor5
+import DataPreprocessor6
 
 if __name__ == '__main__':
-    modelName="--dbcnbilstm"
-    trainOrTest="--train"
-    filePath="--"
+    dataset = str(sys.argv[1])
+    combination = str(sys.argv[2])
+    modelName = str(sys.argv[3])
+    trainOrTest = str(sys.argv[4])
+    filePath = str(sys.argv[5])
+
+    dataPreprocessor=None
+    testModelSavingPath=""
+
+    if dataset=="--twitter":
+        if combination=="--sourceposts":
+            dataPreprocessor = DataPreprocessor1.DataPreprocessor1()
+            testModelSavingPath = "./models/networks/"
+        elif combination=="--sourceposts-replies":
+            dataPreprocessor = DataPreprocessor11.DataPreprocessor11()
+            testModelSavingPath = "./models/networks/"
+        elif combination == "--sourceposts-reposts":
+            dataPreprocessor = DataPreprocessor2.DataPreprocessor2()
+            testModelSavingPath = "./models2/networks/"
+        elif combination == "--sourceposts-replies-reposts":
+            dataPreprocessor = DataPreprocessor3.DataPreprocessor3()
+            testModelSavingPath = "./models3/networks/"
+    elif dataset=="--weibo":
+        if combination == "--sourceposts":
+            dataPreprocessor = DataPreprocessor4.DataPreprocessor4()
+            testModelSavingPath = "./models4/networks/"
+        elif combination == "--sourceposts-replies":
+            dataPreprocessor = DataPreprocessor41.DataPreprocessor41()
+            testModelSavingPath = "./models4/networks/"
+        elif combination == "--sourceposts-reposts":
+            dataPreprocessor = DataPreprocessor5.DataPreprocessor5()
+            testModelSavingPath = "./models5/networks/"
+        elif combination == "--sourceposts-replies-reposts":
+            dataPreprocessor = DataPreprocessor6.DataPreprocessor6()
+            testModelSavingPath = "./models6/networks/"
 
     sess = tf.Session()
-    dataPreprocessor = DataPreprocessor1.DataPreprocessor1()
     if trainOrTest=="--train":
         if modelName=="--dbcn":
             print("Start Training DBCN!")
@@ -32,52 +71,50 @@ if __name__ == '__main__':
             print("Start Training DBCN_BiLSTM!")
             dbcnbilstm=DBCN_BiLSTM.DBCN_BiLSTM()
             if filePath=="--":
-                DBCN_BiLSTM.DBCN_BiLSTM.modelSavingPath="./models/networks/gru2/"
-                # DBCN.DBCN_Model.modelSavingPath = "./models_Twitter/networks/dbcnbilstm_withoutcomments/"
+                DBCN_BiLSTM.DBCN_BiLSTM.modelSavingPath="./models/networks/dbcnbilstm/"
             else:
                 DBCN_BiLSTM.DBCN_BiLSTM.modelSavingPath=filePath
             dbcnbilstm.modelSavingPath=filePath
             dbcnbilstm.train(sess,dataPreprocessor)
-            print("DBCN_BiLSTM! Training Completed!")
-        # elif modelName=="--odcn":
-        #     print("Start Training ODCN!")
-        #     odcn = ODCN_Model.ODCN_Model()
-        #     if filePath == "--":
-        #         ODCN_Model.ODCN_Model.modelSavingPath = "./trained models/new networks/odcn/"
-        #     else:
-        #         ODCN_Model.ODCN_Model.modelSavingPath = filePath
-        #     odcn.modelSavingPath = filePath
-        #     odcn.train(sess, dataPreprocessor)
-        #     print("ODCN Training Completed!")
+            print("DBCN_BiLSTM Training Completed!")
+        elif modelName == "--bilstm":
+            print("Start Training BiLSTM!")
+            dbcnbilstm = BiLSTM.BiLSTM()
+            if filePath == "--":
+                BiLSTM.BiLSTM.modelSavingPath = "./models/networks/bilstm/"
+            else:
+                BiLSTM.BiLSTM.modelSavingPath = filePath
+            dbcnbilstm.modelSavingPath = filePath
+            dbcnbilstm.train(sess, dataPreprocessor)
+            print("BiLSTM Training Completed!")
     elif trainOrTest=="--test":
         if modelName=="--dbcn":
             print("Start Testing DBCN!")
             dbcn=DBCN.DBCN_Model()
-            if filePath=="--":
-                DBCN.DBCN_Model.modelSavingPath="./models/networks/dbcn/"
+            if combination=="--sourceposts":
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath+"/dbcn_withoutcomments/"
             else:
-                DBCN.DBCN_Model.modelSavingPath=filePath
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath + "/dbcn/"
             dbcn.modelSavingPath=filePath
             dbcn.test(sess,dataPreprocessor)
             print("DBCN Test Completed!")
         elif modelName=="--dbcnbilstm":
             print("Start Testing DBCN_BiLSTM!")
-            dbcn=DBCN_BiLSTM.DBCN_BiLSTM()
-            if filePath=="--":
-                # DBCN.DBCN_Model.modelSavingPath="./models_Twitter/networks/dbcnbilstm_withoutcomments/"
-                DBCN.DBCN_Model.modelSavingPath = "./models/networks/gru2/"
+            dbcn = DBCN.DBCN_Model()
+            if combination == "--sourceposts":
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath + "/dbcnbilstm_withoutcomments/"
             else:
-                DBCN.DBCN_Model.modelSavingPath=filePath
-            dbcn.modelSavingPath=filePath
-            dbcn.test(sess,dataPreprocessor)
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath + "/dbcnbilstm/"
+            dbcn.modelSavingPath = filePath
+            dbcn.test(sess, dataPreprocessor)
             print("DBCN_BiLSTM Test Completed!")
-        # elif modelName=="--odcn":
-        #     print("Start Testing ODCN!")
-        #     odcn = ODCN_Model.ODCN_Model()
-        #     if filePath == "--":
-        #         ODCN_Model.ODCN_Model.modelSavingPath = "./trained models/networks/odcn/"
-        #     else:
-        #         ODCN_Model.ODCN_Model.modelSavingPath = filePath
-        #     odcn.modelSavingPath = filePath
-        #     odcn.test(sess, dataPreprocessor)
-        #     print("ODCN Test Completed!")
+        elif modelName == "--bilstm":
+            print("Start Testing BiLSTM!")
+            dbcn = DBCN.DBCN_Model()
+            if combination == "--sourceposts":
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath + "/bilstm_withoutcomments/"
+            else:
+                DBCN.DBCN_Model.modelSavingPath = testModelSavingPath + "/bilstm/"
+            dbcn.modelSavingPath = filePath
+            dbcn.test(sess, dataPreprocessor)
+            print("BiLSTM Test Completed!")
